ee9abe296afd4302fae084b0993a12d8c933e520
Moyez <moyezrabbani@gmail.com>
2024-01-29 23:50:09 +0530

    started working in auth

---
+++ app/auth/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+
+
+const AuthLayout = ({ children }: {
+    children: ReactNode
+}) => {
+  return (
+    <div className='h-full flex items-center justify-center bg-sky-500'>{children}</div>
+  )
+}
+
+export default AuthLayout
